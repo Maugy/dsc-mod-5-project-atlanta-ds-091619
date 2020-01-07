e6e9add46f84faac30d4ce51e58b32b6dccde479
David M <dmauger@gmail.com>
2020-01-06 20:19:46 -0500

    dm

--- FinalProject copy/mapbox.py
+++
@@ -1,3 +0,0 @@
-mapbox_access_token = 'pk.eyJ1IjoiZG1hdWdlciIsImEiOiJjazE4OXhuYmgxZ3d2M21uaXV5eWt6Nm5iIn0.8mXDhVPh7XaS9VSr1Uj7cw'
-# chart_studio.tools.set_credentials_file(username='dmauger', api_key='AikWpcnd27N8lxreryv3')
-
